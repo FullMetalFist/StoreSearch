6a2ed8f0ef8fb9f72329723d1d5e43e560281bbf
Michael Vilabrera <m.vilabrera@gmail.com>
2018-04-11 10:32:55 -0400

    initial commit

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SearchViewController.swift
+//  StoreSearch
+//
+//  Created by Michael Vilabrera on 4/11/18.
+//  Copyright © 2018 Michael Vilabrera. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
