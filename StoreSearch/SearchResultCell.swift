de9c11c6e78ec8d7ebc13db88b7d9a963948d41a
Michael Vilabrera <m.vilabrera@gmail.com>
2018-04-16 10:31:14 -0400

    add SearchResultCell.swift file to project

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Michael Vilabrera on 4/16/18.
+//  Copyright © 2018 Michael Vilabrera. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
