0783277b0144574d6ffb08163b729de2e489fcfa
Michael Vilabrera <m.vilabrera@gmail.com>
2018-04-18 16:44:05 -0400

    add properties to SearchResult object & enable CodingKey support

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -16,12 +16,29 @@ class ResultArray: Codable {
 class SearchResult: Codable, CustomStringConvertible {
     var artistName = ""
     var trackName = ""
+    var kind = ""
+    
+    var trackPrice = 0.0
+    var currency = ""
+    var imageSmall = ""
+    var imageLarge = ""
+    var storeURL = ""
+    var genre = ""
     
     var name: String {
         return trackName
     }
     
     var description: String {
-        return "Name: \(name), Artist Name: \(artistName)"
+        return "Kind: \(kind), Name: \(name), Artist Name: \(artistName)"
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case imageSmall = "artworkUrl60"
+        case imageLarge = "artworkUrl100"
+        case storeURL = "trackViewUrl"
+        case genre = "primaryGenreName"
+        case kind, artistName, trackName
+        case trackPrice, currency
     }
 }
