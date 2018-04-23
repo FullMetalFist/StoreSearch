ea85b9b129ace9ce377fc7b550fb50e76de4132c
Michael Vilabrera <m.vilabrera@gmail.com>
2018-04-23 16:35:41 -0400

    further refactor and reduce

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -87,9 +87,7 @@ extension SearchViewController: UISearchBarDelegate {
             if let data = performStoreRequest(with: url) {
                 searchResults = parse(data: data)
             }
-            searchResults.sort {
-                $0 < $1
-            }
+            searchResults.sort(by: <)
             tableView.reloadData()
         }
     }
