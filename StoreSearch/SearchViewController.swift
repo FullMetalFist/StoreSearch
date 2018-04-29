29591429b8d01c498bb086c4e50a46334efb8c1f
Michael Vilabrera <m.vilabrera@gmail.com>
2018-04-29 18:53:15 -0400

    add prepareForSegue in SearchViewController

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -122,6 +122,16 @@ class SearchViewController: UIViewController {
             dataTask?.resume()
         }
     }
+    
+    // MARK: Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowDetail" {
+            let detailViewController = segue.destination as! DetailViewController
+            let indexPath = sender as! IndexPath
+            let searchResult = searchResults[indexPath.row]
+            detailViewController.searchResult = searchResult
+        }
+    }
 }
 
 extension SearchViewController: UISearchBarDelegate {
