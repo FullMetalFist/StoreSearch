6903d50a29705a6488b36191a3e7da581ed153c2
Michael Vilabrera <m.vilabrera@gmail.com>
2018-04-30 16:40:57 -0400

    add SlideOutAnimationController to project

---
+++ StoreSearch/SlideOutAnimationController.swift
@@ -0,0 +1,9 @@
+//
+//  SlideOutAnimationController.swift
+//  StoreSearch
+//
+//  Created by Michael Vilabrera on 4/30/18.
+//  Copyright © 2018 Michael Vilabrera. All rights reserved.
+//
+
+import Foundation
