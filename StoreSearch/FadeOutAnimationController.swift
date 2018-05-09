17471899472c785d40614ddc01196c5c61f5bddf
Michael Vilabrera <m.vilabrera@gmail.com>
2018-05-09 14:55:02 -0400

    add FadeOutAnimationController file to project

---
+++ StoreSearch/FadeOutAnimationController.swift
@@ -0,0 +1,9 @@
+//
+//  FadeOutAnimationController.swift
+//  StoreSearch
+//
+//  Created by Michael Vilabrera on 5/9/18.
+//  Copyright © 2018 Michael Vilabrera. All rights reserved.
+//
+
+import Foundation
