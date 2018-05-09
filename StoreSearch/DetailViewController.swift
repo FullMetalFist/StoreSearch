144eb97c8f476e0787054fe0979114cfa056ecf2
Michael Vilabrera <m.vilabrera@gmail.com>
2018-05-09 15:09:33 -0400

    implement transition animation for DetailViewController & LandscapeViewController

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -20,6 +20,12 @@ class DetailViewController: UIViewController {
     
     var searchResult: SearchResult!
     var downloadTask: URLSessionDownloadTask?
+    var dismissStyle = AnimationStyle.fade
+    
+    enum AnimationStyle {
+        case slide
+        case fade
+    }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -44,6 +50,7 @@ class DetailViewController: UIViewController {
     }
     
     @IBAction func close() {
+        dismissStyle = .slide
         dismiss(animated: true, completion: nil)
     }
     
@@ -101,7 +108,12 @@ extension DetailViewController: UIViewControllerTransitioningDelegate {
     }
     
     func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return SlideOutAnimationController()
+        switch dismissStyle {
+        case .slide:
+            return SlideOutAnimationController()
+        case .fade:
+            return FadeOutAnimationController()
+        }
     }
 }
 
